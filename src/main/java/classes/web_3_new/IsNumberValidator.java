e7e6250f64aad72c69790a3b51b7ba4464157048
Мартышов Данила Викторович <409091@niuitmo.ru>
2024-11-05 00:39:29 +0300

    TO-DO: small tweaks

---
+++ src/main/java/classes/web_3_new/IsNumberValidator.java
@@ -0,0 +1,21 @@
+package classes.web_3_new;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.FacesValidator;
+import jakarta.faces.validator.Validator;
+import jakarta.faces.validator.ValidatorException;
+
+@FacesValidator("isNumberValidator")
+public class IsNumberValidator implements Validator {
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        try {
+            Double.parseDouble(value.toString());
+        }catch (NumberFormatException e){
+            FacesMessage fm = new FacesMessage("Input must only include numbers");
+            throw new ValidatorException(fm);
+        }
+    }
+}
